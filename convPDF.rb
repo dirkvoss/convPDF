35813de6971a2363e78bec070989747b96488337
dirkvoss <mail@dirk-voss.com>
2020-01-27 18:56:36 +0100

    Searchpattern and Moving of files

--- convPDF.rb
+++ convPDF.rb
@@ -6,14 +6,15 @@ require 'evernote_uploader'
 require 'pdf-reader'
 
 map2Dir = { 
-						"Vattenfall" 	=> "GasWasserStrom", 
-						"ARI Fleet Germany"		=> "Leasing" 
+						"Vattenfall" 	    => "GasWasserStrom", 
+            "Geburtsdatum"		=> "GasWasserStrom" ,
+            "gibt es bestimmt nicht" => "na"
 					}
 
 maindir='/mnt/freenas/02_users/dirk/convPDF/'
 ocrbin='/usr/bin/ocrmypdf --force-ocr -l deu '
 indir='/mnt/freenas/07_Dokumente/Scan/Inbox/Scanned/'
-outdir=' /mnt/freenas/07_Dokumente/Scan/Inbox/ScannedOCR/'
+outdir='/mnt/freenas/07_Dokumente/Scan/Inbox/ScannedOCR/'
 errdir='/mnt/freenas/07_Dokumente/Scan/Inbox/ScannedError/'
 movedir='/mnt/freenas/07_Dokumente/Scan/'
 
@@ -25,23 +26,23 @@ log = Logger.new(logfile, 'monthly')
 
 loop do
   log.debug "Start"
-  log.debug "Kein PDF FIle in #{indir} vorhanden" if Dir.empty?(indir)
+  log.debug "No PDF file in  #{indir} " if Dir.empty?(indir)
 
   Dir.glob('*.pdf', base: indir) do |file|
     infile=indir + file
-    cmd=ocrbin + indir + file + outdir + file + " 2>&1"
+    cmd=ocrbin + indir + file + ' ' + outdir + file + " 2>&1"
     execute=`#{cmd}`
     if $?.success?
-	    log.debug "#{file} erfolgreich in #{outdir} erzeugt"
+	    log.debug "#{file} generated in #{outdir} "
 	    if File.exist?(infile)
               File.delete(infile) 
-	      log.debug "File #{file} in #{indir} geloescht"
+	      log.debug "#{file} in #{indir} deleted"
 	    else
-	      log.error "File #{file} konnte in #{indir} nicht gefunden werden"	  
+	      log.error "#{file} not found in #{indir}"	  
       end  
    
       outfile=outdir + file	   
-      log.debug "File #{outfile} will be scanned"
+      log.debug "#{outfile} will be scanned"
       reader = PDF::Reader.new(outfile.strip)
 
       #hash leeren
@@ -52,9 +53,9 @@ loop do
 
       reader.pages.each do |page|
         map2Dir.each do |key, value|
-          log.debug "check #{key}"
+          #log.debug "check #{key}"
           if page.text.match /#{key}/ 
-            log.debug "#{key} found"
+            #log.debug "#{key} found"
             found_hash[key]+=1
           end
         end
@@ -64,18 +65,28 @@ loop do
       maxkey=''
       map2Dir.each do |key, value|
         if found_hash[key] > 0
-          log.debug "#{key}  found  #{found_hash[key]} times in #{file}"
+          log.debug "Searchpattern \"#{key}\" found #{found_hash[key]} times in #{file}"
           if found_hash[key] > maxCnt
             maxCnt=found_hash[key]
             maxkey=key
           end
         else
-          log.debug "#{key} not found in #{file}"
+          log.debug "Searchpattern \"#{key}\" not found in #{file}"
         end
       end
 
-      log.debug "Max #{maxkey} found #{found_hash[maxkey]} times in #{file}"
-     
+      log.debug "The searchpattern \"#{maxkey}\" with #{found_hash[maxkey]} hits was the maximum in #{file}"
+      destdir=movedir+map2Dir[maxkey]
+      log.debug "File will be moved to #{destdir}"
+      if Dir.exists?(destdir)
+        log.debug "#{destdir} exists - file #{outfile} will be moved"
+        FileUtils.move outfile, destdir
+      else
+        log.debug "#{destdir} does not exist and will be created"
+        Dir.mkdir(destdir,755) 
+        FileUtils.move outfile, destdir
+      end
+
     else
       log.error "#{file} konnte nicht umgewandelt werden !"
       log.error "#{execute.chomp}"
